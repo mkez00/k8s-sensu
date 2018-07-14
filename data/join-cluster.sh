ef1140704ec1e4e9853af41a5edcf55b18ded41c
Michael Kezele <michael.kezele@scenedoc.com>
2018-07-14 12:45:03 -0400

    updated readme since shell provisioners start all services correctly

--- data/join-cluster.sh
+++ data/join-cluster.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.10:6443 --token q8bqsv.izadrg6n0p5651jr --discovery-token-ca-cert-hash sha256:b1c8388884234a1ef7b2cbba038cf34e3789acd691ecf830e38b861eed6f7bac
+kubeadm join 192.168.56.10:6443 --token tbhg3m.bl8bhzahc337mlvs --discovery-token-ca-cert-hash sha256:42a36c19bcdda01fc970be2aae388178c6d0bc3e127b0263b5a2b970291a759b
